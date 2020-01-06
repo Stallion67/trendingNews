f489b96b6f6b3899bea76d4083a7066ec7cfe37a
Stallion67 <mwakaben@gmail.com>
2020-01-06 17:26:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/trending/RepoListFragment.kt
@@ -0,0 +1,9 @@
+package com.example.trending
+private lateinit var viewDataBinding: FragmentRepoListBinding
+private lateinit var adapter: RepoListAdapter
+
+class RepoListFragment {
+    private lateinit var viewDataBinding: FragmentRepoListBinding
+    private lateinit var adapter: RepoListAdapter
+
+}
